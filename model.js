a87cb5afb9ff87bee78bad6c5c8863d4071d7045
marioeguiluz <mario@myplacesandme.com>
2017-01-28 20:04:25 +0000

    show array of items in search

--- model.js
+++ model.js
@@ -7,9 +7,14 @@ function Facility(name,phone,website,email,latitude,longitude) {
 
 Facility.prototype = {
     initialize: function(name,phone,website,email,latitude,longitude) {
+        var secureWebsite = website ? website.replace("http", "https") : "https://www.google.co.uk/";
+        if (!secureWebsite.startsWith("https")) {
+            secureWebsite = "https://" + secureWebsite;
+        }
+
         this.name = name;
         this.phone = phone;
-        this.website = website ? website.replace("http", "https") : "https://www.google.co.uk/";
+        this.website = secureWebsite;
         this.email = email;
         this.latitude = latitude;
         this.longitude = longitude;
