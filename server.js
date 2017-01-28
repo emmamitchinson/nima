fe5a834bba1fa2f7ed84ddb0506aed53e41e0cac
Emma Mitchinson <emmamitchinson@hotmail.co.uk>
2017-01-28 16:29:46 +0000

    refactor more

--- server.js
+++ server.js
@@ -22,8 +22,10 @@ var BOT_RESPONSES  = {
 };
 
 var BOT_STATUS = {
-    NEED_LOCATION : 1,
-    MENU : 2
+    NEED_GREET : 0,
+    NEED_LANG : 1,
+    NEED_LOCATION : 2,
+    MENU : 3
 };
 
 var BOT_SEARCH_OPTIONS = {
@@ -39,7 +41,7 @@ var BOT_SEARCH_OPTIONS = {
 var port = process.env.PORT || 8080;
 var token = "EAAESs7ymteEBAHQrZC3y2RQrXswMilWUGjPZBNyIuMVndVgBktVMSbRzEEkWPdnQXvRXdOCPxNDDfRzQ2lo9yXUyYx2y4jFPX3wDSw8yZBSNUX7MtTKB207imhW29ofQUSuFZAacf0ok417RHQZB40JZAkf1lAMO0FvAbdck1XrwZDZD";
 var secret = "nimaInHackathon";
-var status = BOT_STATUS.NEED_LOCATION;
+var status = BOT_STATUS.NEED_GREET;
 var lat = 0;
 var lng = 0;
 var currentLang = null;
@@ -88,16 +90,20 @@ app.post('/webhook/', function (req, res) {
         console.log(`Available states: ${JSON.stringify(BOT_STATUS)}`);
 
         switch (status) {
-            case BOT_STATUS.NEED_LOCATION:
+            case BOT_STATUS.NEED_GREET:
                 introductoryGreet(sender);
+                break;
+            case BOT_STATUS.NEED_LANG:
                 setLanguageFromQuickReplies(event);
+                break;
+            case BOT_STATUS.NEED_LOCATION:
                 handleNeedLocation(event, sender, req,res);
                 break;
             case BOT_STATUS.MENU:
                 handleMenu(event, sender, req,res);
                 break;
             default:
-                sayError(sender,BOT_STATUS.NEED_LOCATION,res);
+                sayError(sender,BOT_STATUS.NEED_GREET,res);
                 break;
         }
     }
@@ -134,9 +140,6 @@ function handleNeedLocation(event, sender, req,res) {
                             }
                         });
                         break;
-                    // default:
-                    //     sayError(sender, BOT_STATUS.NEED_LOCATION, res);
-                    //     break;
                 }
             } else {
                 sayError(sender, BOT_STATUS.NEED_LOCATION, res);
@@ -264,7 +267,7 @@ function sayReset(sender, res) {
     console.log("******** RESET");
     lat = 0;
     lng = 0;
-    status = BOT_STATUS.NEED_LOCATION;
+    status = BOT_STATUS.NEED_GREET;
     currentLang = null;
     askedLangNoLocation = false;
     replyToSender(sender,BOT_RESPONSES.RESET);
