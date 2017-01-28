b72c1db8aa5cd9574f15f4b283269f4fcf5440d1
marioeguiluz <mario@myplacesandme.com>
2017-01-28 14:35:06 +0000

    Typing dots on long calls

--- server.js
+++ server.js
@@ -160,7 +160,6 @@ function handleMenu(event, sender, req,res) {
 
                     case BOT_SEARCH_OPTIONS.HOSPITALS.toLowerCase():
                         showTyping(true, sender);
-                        res.sendStatus(200);
                         apis.getNHSFacility(apis.searchTypes.HOSPITALS,lat,lng,function(name){
                             replyToSender(sender,name);
                             showTyping(false, sender);
@@ -398,13 +397,18 @@ setLanguageFromQuickReplies = (event) => {
 }
 
 function showTyping(flag,sender) {
+    typing = "typing_off";
+    if (flag == true ) {
+        typing = "typing_on";
+    } 
+
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
         qs: { access_token : token },
         method: 'POST',
         json: {
             recipient: { id : sender },
-            sender_action: flag == true ? "typing_on" : "typing_off"
+            sender_action: typing
         }
     }, function(error, response, body) {
         if (error) {
