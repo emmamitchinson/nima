1fcfcaf99f16996039ea51efe6a48dd0651a8d73
Emma Mitchinson <emmamitchinson@hotmail.co.uk>
2017-01-29 10:33:04 +0000

    add language file

---
+++ responses.js
@@ -0,0 +1,37 @@
+module.exports = {
+	en_GB: {
+		INTRODUCTION : "Hi, I'm NIMA. I'm your smart friend. I can find NHS facilities near you. And this is only the beginning.",
+	    INPUT_GREET1 : 'hi',
+	    INPUT_GREET2 : 'hello',
+	    INPUT_GREET3 : 'hey',
+	    RESET : 'Lets do a fresh start...',
+	    INPUT_RESET : 'reset',
+	    GREETING: 'Hi, ',
+	    GREETING_POST: ' nice to see you here :)',
+	    LOCATION: 'Ready to go! Now send me your location or write me your postcode to help you out.',
+	    THANKS: "Thank you (Y)",
+	    SEARCH_OPTIONS: "Thank you (Y). Now I can find for you the nearest...",
+	    SEARCH_OPTIONS_REPEAT: "Of course :D! Let me find for you the nearest...",
+	    ERROR: 'That is so funny! :D',
+	    INVALID_POSTCODE: "Mmm... It doesn't look like a valid postcode, do you want to give another try?",
+	    LANG_SET: "I've set your language to",
+	    LANG_CHANGE: ', would you like to change it?'
+	},
+	fr_FR: {
+		INTRODUCTION : "Salut, je m'appelle NIMA. Je suis votre ami habile. Je peux trouver les aménagement de NHS près de vous. Ce n'est que le début",
+	   	INPUT_GREET1 : 'salut',
+	    INPUT_GREET2 : 'bonjour',
+	    INPUT_GREET3 : 'tiens',
+	    RESET: "Recommençons...",
+		GREETING: "Salut, ",
+		GREETING_POST: "Ravi de vous voir :)",
+		LOCATION: "Bien, m'envoye votre adresse ou votre code postal pour obtenir des informations sur votre zone",
+		THANKS: "Merci",
+		SEARCH_OPTIONS: "Merci beaucoup. Maintenant je peux trouver le plus proche...",
+		SEARCH_OPTIONS_REPEAT: "Bien sûr :D! Laissez moi trouver le plus proche...",
+		ERROR: "Desolée, pouvez-vous cela d'une manière différente?"
+		INVALID_POSTCODE: "Il semble que c'est une code postal invalide, voulez-vous essayer à nouveau?",
+	    LANG_SET: "J'ai règlé votre langue à",
+	    LANG_CHANGE: ', voulez-vous le modifier?'
+	}
+}
